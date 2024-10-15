94ccf3e833eae5275e4a231737ad1b8ce979d1db
Kevin Ruiz <keleorula@gmail.com>
2024-10-14 22:16:57 -0500

    Proyecto Vsystem final

--- estudiante_curso/views.py
+++ estudiante_curso/views.py
@@ -5,31 +5,32 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from curso.models import curso  
 
-# Vista para listar los estudiantes y cursos
+
 def Estudiante_Curso(request):
-    cursos = curso.objects.all()  # Obtener todos los cursos disponibles
-    curso_seleccionado = request.GET.get('curso')  # Obtener el curso seleccionado del filtro (si hay)
+    cursos = curso.objects.all()  
+    curso_seleccionado = request.GET.get('curso')  
 
     if curso_seleccionado:
-        estudiantes_cursos = EstudianteCurso.objects.filter(curso=curso_seleccionado)  # Filtrar por curso seleccionado
+        estudiantes_cursos = EstudianteCurso.objects.filter(curso=curso_seleccionado)  
     else:
-        estudiantes_cursos = EstudianteCurso.objects.all()  # Mostrar todos si no se selecciona un curso
+        estudiantes_cursos = EstudianteCurso.objects.all()  
 
     return render(request, 'lista_est_cur.html', {
         'title': 'Relación estudiantes y curso',
         'estudiantes_cursos': estudiantes_cursos,
-        'cursos': cursos,  # Pasar los cursos a la plantilla
-        'curso_seleccionado': curso_seleccionado  # Pasar el curso seleccionado a la plantilla
+        'cursos': cursos,  
+        'curso_seleccionado': curso_seleccionado  
     })
-# Vista para agregar o editar una relación de estudiante con curso
+
+
 def formulario_estudiante_curso(request):
     if request.method == 'POST':
         form = EstudianteCursoForm(request.POST)
         if form.is_valid():
-            form.save()  # Guarda el formulario
-            return redirect('lista-estudiantes-cursos')  # Asegúrate de que este nombre coincida
+            form.save()  
+            return redirect('lista-estudiantes-cursos')  
     else:
-        form = EstudianteCursoForm()  # Formulario vacío si no es POST
+        form = EstudianteCursoForm()  
 
     return render(request, 'formulario_estudiante_curso.html', {'form': form})
 
